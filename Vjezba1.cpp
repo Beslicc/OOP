03bdbe4197c5ed7c3e8752cbe092ed4bae0cd8bf
Beslicc <beslicjosip0@gmail.com>
2025-10-19 15:16:35 +0200

    Add files via upload

---
+++ Vjezba1.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cctype>
+
+/*
+int main()
+{
+	int a, b, s;
+	std::cout << "unesi dva broja" << std::endl;
+	std::cin >> a >> b;
+	s = a + b;
+	std::cout << "suma je " << s << std::endl;
+}
+*/
+/*
+//1
+int main()
+{
+	bool flag;
+	std::cout << "unesi " << true << " - " << false << std::endl;
+	std::cin >> flag;
+	std::cout << std::uppercase << flag << std::endl;
+	int a = 255;
+	std::cout << "hex " << std::hex << a << std::endl;
+	std::cout << "dec " << std::dec << a << std::endl;
+	std::cout << "oct " << std::oct << a << std::endl;
+	double pi = 3.141592;
+	std::cout << "pi = " << std::fixed << std::uppercase;
+	std::cout << std::setprecision(10) << std::setw(20);
+	std::cout << std::setfill('0');
+	std::cout << pi << std::endl;
+	system("pause");
+}
+*/
+
+/*
+//2.
+using namespace std;
+
+int limit(int vrijednost, int low = 0, int high = 100) {
+	if (vrijednost < low)
+		return low;
+	else if (vrijednost >= high)
+		return high;
+	else
+		return vrijednost;
+}
+
+double limit(double vrijednost, double low = 0.0, double high = 100.0) {
+	if (vrijednost < low)
+		return low;
+	else if (vrijednost >= high)
+		return high;
+	else
+		return vrijednost;
+}
+
+int main() {
+	int vrijednost;
+	cout << "unesi broj:" << endl;
+	cin >> vrijednost;
+	int rezultat = limit(vrijednost);
+
+	cout << "vrijednost: " << rezultat << endl;
+	return 0;
+}
+*/
+
+/* 
+// ZAD 3
+int main()
+{
+	std::string s;
+	std::cout << "Unesi tekst: ";
+	std::getline(std::cin, s);
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		else if (s[i] >= '0' && s[i] <= '9')
+			s[i] = '*';
+		else if (s[i] == ' ')
+			s[i] = '_';
+	}
+
+	std::cout << "Novi tekst: " << s << std::endl;
+	return 0;
+}
+*/
+
+/*
+// ZAD 4
+int& at(int arr[], int index) {
+	return arr[index];
+}
+
+int main() {
+	int a[5] = { 1, 2, 3, 4, 5 };
+	at(a, 2)++;
+	for (int i = 0; i < 5; ++i) std::cout << a[i] << (i + 1 < 5 ? ' ' : '\n');
+	return 0;
+}
+*/
